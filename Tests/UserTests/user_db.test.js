aa1413d2c4d21be61ad6642a07e6a8eae3db5b09
Ichoku Chinonso <greatchinonso7@gmail.com>
2020-02-19 09:09:17 +0100

    tests for validating user

--- Tests/UserTests/user_db.test.js
+++ Tests/UserTests/user_db.test.js
@@ -5,6 +5,9 @@ const {
 
 describe('User DB Test Suite', () => {
     it("should see if a username already exists in db", async () => {
-
+        const check = await UsernameExists('sdfgdf');
+        expect(check).to.be.false
+        expect(check === undefined).to.be.false
+        expect(check === null).to.be.false
     })
 })
